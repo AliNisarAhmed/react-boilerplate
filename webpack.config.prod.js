c6991acf8c2eecc8d89b60b680f6ea8950d46c9d
Ali Ahmed <ali.ahmed@punchcard.io>
2020-01-19 14:28:03 -0700

    Using webpack bundle analyzer and running npm command from within another

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,12 @@
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.config.base');
 
 module.exports = merge(baseConfig, {
-	mode: 'production'
+	mode: 'production',
+	plugins: [new BundleAnalyzerPlugin({
+		analyzerMode: 'static',
+		openAnalyzer: false,
+		reportFilename: 'bundle_sizes.html'
+	})]
 });
