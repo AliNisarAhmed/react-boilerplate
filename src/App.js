1c72cb3fa18aa17d4708fb9d39154a591117fbe0
Ali Ahmed <ali.ahmed@punchcard.io>
2020-01-28 19:37:46 -0700

    Add babel plugin for dyamic import, to enable lazy loading components using React.lazy

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './styles.css';
 
 import { hot } from 'react-hot-loader';
 
+const Warning = React.lazy(() => import('./Warning'));
+
 class App extends React.Component {
 
 	state = {
@@ -10,12 +12,18 @@ class App extends React.Component {
 	}
 
 	render () {
+		const { count } = this.state;
 		return (
 			<div>
 				<h1>Hello World!!!!</h1>
 				<h2>Count: {this.state.count}</h2>
 				<button onClick={() => this.setState(s => ({ count: s.count + 1}))}>+</button>
 				<button onClick={() => this.setState(s => ({ count: s.count - 1}))}>-</button>
+				{count > 10 ? (
+					<React.Suspense fallback={null}>
+						<Warning />
+					</React.Suspense>
+				) : null}
 			</div>
 		)
 	}
